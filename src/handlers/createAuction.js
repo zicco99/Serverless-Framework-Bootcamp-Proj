296070f1526915a04311409421207b1e98dd2042
Francesco Ziccolella <f.ziccolella@protonmail.com>
2024-08-13 21:14:59 +0200

    add middy and error handling

--- src/handlers/createAuction.js
+++ src/handlers/createAuction.js
@@ -1,5 +1,10 @@
 import { v4 as uuid } from 'uuid';
 import { AWS } from 'aws-sdk';
+import middy from '@middy/core';
+import httpJsonBodyParser from '@middy/http-json-body-parser';
+import httpErrorHandler from '@middy/http-error-handler';
+import httpEventNormalizer from '@middy/http-event-normalizer';
+import createError from 'http-errors';
 
 const dynamodb = new AWS.DynamoDB.DocumentClient();
 
@@ -12,10 +17,15 @@ async function createAuction(event, context) {
     createdAt: new Date().toISOString()
   };
 
-  await dynamodb.put({
-    TableName: process.env.AUCTIONS_TABLE_NAME,
-    Item: auction
-  }).promise();
+  try{
+    await dynamodb.put({
+      TableName: process.env.AUCTIONS_TABLE_NAME,
+      Item: auction
+    }).promise();
+  } catch(error) {
+    console.error(error);
+    throw new createError.InternalServerError(error);
+  }
 
   return {
     statusCode: 201,
@@ -23,6 +33,9 @@ async function createAuction(event, context) {
   };
 }
 
-export const handler = createAuction;
+export const handler = middy(createAuction)
+  .use(httpJsonBodyParser())
+  .use(httpEventNormalizer())
+  .use(httpErrorHandler());
 
 
