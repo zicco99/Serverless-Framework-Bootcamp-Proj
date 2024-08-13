1baba4b1cec016df251d8d4e9247f5d7735097a3
Francesco Ziccolella <f.ziccolella@protonmail.com>
2024-08-13 12:59:24 +0200

    dynamodb summary

--- src/handlers/createAuction.js
+++ src/handlers/createAuction.js
@@ -1,8 +1,16 @@
 async function createAuction(event, context) {
+  const {title} = JSON.parse(event.body);
+  const auction = {
+    id: uuid.v4(),
+    title: title,
+    status: 'OPEN',
+    createdAt: new Date().toISOString()
+  }
+
   return {
-    statusCode: 200,
-    body: JSON.stringify({ event, context }),
-  };
+    statusCode: 201,
+    body: JSON.stringify(auction),
+  }
 }
 
 export const handler = createAuction;
