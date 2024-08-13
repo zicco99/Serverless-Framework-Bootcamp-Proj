863bd0f2a7f5b9cea5f9bebc4ee82da05a995608
Francesco Ziccolella <f.ziccolella@protonmail.com>
2024-08-13 21:55:38 +0200

    fix

--- src/lib/commoMiddleware.js
+++ src/lib/commoMiddleware.js
@@ -1,11 +1,15 @@
-import middy from "@middy/core";
-import httpJsonBodyParser from "@middy/http-json-body-parser";
-import httpEventNormalizer from "@middy/http-event-normalizer";
-import httpErrorHandler from "@middy/http-error-handler";
+import middy from '@middy/core';
+import httpJsonBodyParser from '@middy/http-json-body-parser';
+import httpEventNormalizer from '@middy/http-event-normalizer';
+import httpErrorHandler from '@middy/http-error-handler';
 
+/**
+ * Wraps a Lambda handler with common Middy middleware.
+ * @param {Function} handler - The Lambda function handler.
+ * @returns {Function} - The Middy-wrapped handler.
+ */
 export default (handler) =>
-  middy(handler).use([
-    httpJsonBodyParser(),
-    httpEventNormalizer(),
-    httpErrorHandler(),
-  ]);
+  middy(handler)
+    .use(httpJsonBodyParser())
+    .use(httpEventNormalizer())
+    .use(httpErrorHandler());
