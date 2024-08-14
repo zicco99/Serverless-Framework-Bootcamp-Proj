0669a92b5e7c7f97928549ffa35358707069440b
Francesco Ziccolella <f.ziccolella@protonmail.com>
2024-08-14 12:26:57 +0200

    authorizer added

---
+++ src/authorizer/authorizer.js
@@ -0,0 +1,16 @@
+export const handler = async (event) => {
+    //Stuff here
+    return {
+      principalId: 'user',
+      policyDocument: {
+        Version: '2012-10-17',
+        Statement: [
+          {
+            Action: 'execute-api:Invoke',
+            Effect: 'Allow',
+            Resource: event.methodArn,
+          },
+        ],
+      },
+    };
+  };
