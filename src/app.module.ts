76bca06055ccd1e64520619dee68a438c22b607b
Francesco Ziccolella <f.ziccolella@protonmail.com>
2024-08-17 15:44:58 +0200

    auctions_module will now be loaded by main module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AuctionsModule } from './auctions/auctions.module';
 
 @Module({
-  imports: [],
+  imports: [AuctionsModule],
   controllers: [AppController],
   providers: [AppService],
 })
