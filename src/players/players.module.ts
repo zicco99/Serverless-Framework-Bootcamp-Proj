e2522b116f4f9fd0cf37083f7ac736e7733fe20b
Francesco Ziccolella <f.ziccolella@protonmail.com>
2024-08-21 12:54:58 +0200

    bloom filter service added

--- src/players/players.module.ts
+++ src/players/players.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { PlayersController } from './players.controller';
 import { PlayersService } from './players.service';
+import { BloomFilterManagerService } from 'src/lib/bloom-filter.service';
 
 @Module({
   controllers: [PlayersController],
-  providers: [PlayersService],
+  providers: [PlayersService, BloomFilterManagerService],
 })
 export class PlayersModule {}
