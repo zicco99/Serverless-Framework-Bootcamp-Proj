144845e0cbdfa0d7b1b122efb18b87c0566419d7
Francesco Ziccolella <f.ziccolella@protonmail.com>
2024-08-21 13:00:25 +0200

    S3Client provided

--- src/players/players.module.ts
+++ src/players/players.module.ts
@@ -1,10 +1,22 @@
 import { Module } from '@nestjs/common';
 import { PlayersController } from './players.controller';
 import { PlayersService } from './players.service';
-import { BloomFilterManagerService } from 'src/lib/bloom-filter.service';
+import { BloomFilterManagerService } from '../lib/bloom-filter.service';  // Adjust the path if needed
+import { S3Client } from '@aws-sdk/client-s3';
 
 @Module({
   controllers: [PlayersController],
-  providers: [PlayersService, BloomFilterManagerService],
+  providers: [
+    PlayersService,
+    BloomFilterManagerService,
+    {
+      provide: S3Client,
+      useFactory: () => {
+        return new S3Client({
+          region: process.env.REGION || 'eu-west-1',
+        });
+      },
+    },
+  ],
 })
 export class PlayersModule {}
