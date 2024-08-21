8cb3704ce72f820d4db78ae6919ccc33ee5c6d77
Francesco Ziccolella <f.ziccolella@protonmail.com>
2024-08-21 15:04:53 +0200

    fix

--- src/players/players.service.ts
+++ src/players/players.service.ts
@@ -7,14 +7,11 @@ import { S3HashRing } from '../lib/s3-hash-ring.util';
 
 @Injectable()
 export class PlayersService {
-  private readonly s3Client: S3Client;
   private readonly s3HashRing: S3HashRing<Player, CreatePlayerDto, PlayerUniqueAttributes>;
 
   constructor(
-    s3Client: S3Client,
     private readonly bloomFilterManager: BloomFilterManagerService
   ) {
-    this.s3Client = s3Client;
     this.s3HashRing = new S3HashRing<Player, CreatePlayerDto, PlayerUniqueAttributes>(
       ['fullname', 'score'],
       (createPlayerDto: CreatePlayerDto, id: string): Player => ({
@@ -29,7 +26,6 @@ export class PlayersService {
       }),
       bloomFilterManager
     );
-    this.bloomFilterManager = bloomFilterManager;
   }
 
   async putPlayer(createPlayerDto: CreatePlayerDto): Promise<void> {
@@ -37,39 +33,36 @@ export class PlayersService {
   
     try {
       // Add the player entity to the S3 bucket using the S3 hash ring utility
-      // This includes updating the Bloom filter and uploading the updated list to S3
       await this.s3HashRing.addEntity(createPlayerDto);
     } catch (err: any) {
       console.error(`Failed to put player data: ${err.message}`);
       throw new Error(`Failed to put player with ID ${playerId}: ${err.message}`);
     }
   }
-  
-
 
   async getPlayer(uniqueAttributes: PlayerUniqueAttributes): Promise<Player | undefined> {
     const playerId = this.s3HashRing.createHash(uniqueAttributes);
     const bucket = this.s3HashRing.getBucket(uniqueAttributes);
 
-    if( !bucket ) {
+    if (!bucket) {
       throw new Error(`Bucket for player ${playerId} not found`);
     }
   
     try {
-      const bloomFilter = this.bloomFilterManager.getCache().get(bucket!);
-  
-      if (!(bloomFilter?.has(this.s3HashRing.createHash(uniqueAttributes)))) {
+      const bloomFilter = this.bloomFilterManager.getCache().get(bucket);
+
+      if (!bloomFilter?.has(playerId)) {
         console.log(`Player ID ${playerId} not found in Bloom filter.`);
         throw new NotFoundException('Player ID not found in Bloom filter');
       }
-  
+
       // Use the S3HashRing to find the player
       const player = await this.s3HashRing.findOne(uniqueAttributes);
       if (!player) {
         console.log(`Player ID ${playerId} not found in data store.`);
         throw new NotFoundException('Player not found in data store');
       }
-  
+
       return player;
     } catch (err: any) {
       console.error(`Failed to get player data: ${err.message}`);
@@ -77,18 +70,21 @@ export class PlayersService {
     }
   }
 
-  async getPlayers(uniqueAttributes: PlayerUniqueAttributes, limit: number = 10, nextToken?: string): Promise<{ entities: Player[], nextToken?: string }> {
+  async getPlayers(
+    uniqueAttributes: PlayerUniqueAttributes,
+    limit: number = 10,
+    nextToken?: string
+  ): Promise<{ entities: Player[], nextToken?: string }> {
     try {
       // Use the S3HashRing to find the players
       const result = await this.s3HashRing.findWithContinuationToken(uniqueAttributes, limit, nextToken);
-  
-      const { entities, nextToken: nextContToken} = result;
-  
+
+      const { entities, nextToken: nextContToken } = result;
+
       return { entities, nextToken: nextContToken };
     } catch (err: any) {
       console.error(`Failed to get players data: ${err.message}`);
       throw new Error(`Failed to get players: ${err.message}`);
     }
   }
-
 }
