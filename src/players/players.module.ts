b4f12df3dc6d6b8c5d434e6d2e2db3abab8d3962
Francesco Ziccolella <f.ziccolella@protonmail.com>
2024-08-21 12:15:52 +0200

    fix

--- src/players/players.module.ts
+++ src/players/players.module.ts
@@ -1,8 +1,9 @@
 import { Module } from '@nestjs/common';
-import { PlayersController  } from './players.controller';
+import { PlayersController } from './players.controller';
+import { PlayersService } from './players.service';
 
 @Module({
   controllers: [PlayersController],
-  providers: [PlayersModule],
+  providers: [PlayersService],
 })
 export class PlayersModule {}
