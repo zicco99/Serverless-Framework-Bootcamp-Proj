49ddaaa4929acfe436ba4d97b59cd7be25579b01
Francesco Ziccolella <f.ziccolella@protonmail.com>
2024-08-25 12:06:33 +0200

    httpservice added as dependency

--- src/players/players.module.ts
+++ src/players/players.module.ts
@@ -2,12 +2,14 @@ import { Module } from '@nestjs/common';
 import { PlayersController } from './players.controller';
 import { PlayersService } from './players.service';
 import { SportmonksService } from 'src/services/sport-monk.service';
+import { HttpService } from '@nestjs/axios';
 
 @Module({
   controllers: [PlayersController],
   providers: [
     PlayersService,
-    SportmonksService
+    SportmonksService,
+    HttpService
   ],
 })
 export class PlayersModule {}
