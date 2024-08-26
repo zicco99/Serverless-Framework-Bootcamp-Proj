705ba7522b6de463d39b2770348fe89307cbbc78
Francesco Ziccolella <f.ziccolella@protonmail.com>
2024-08-26 17:16:37 +0200

    service injected

--- src/players/players.module.ts
+++ src/players/players.module.ts
@@ -1,11 +1,12 @@
 import { Module, Global } from '@nestjs/common';
 import { HttpModule } from '@nestjs/axios';
 import { PlayersService } from './players.service';
+import { SportmonksService } from 'src/services/sport-monk.service';
 
 @Global()
 @Module({
   imports: [HttpModule],
-  providers: [PlayersService],
+  providers: [PlayersService, SportmonksService],
   exports: [PlayersService],
 })
 export class PlayersModule {}
