96a5b1ea10639767053d8033ffb53ee4e7533d03
Francesco Ziccolella <f.ziccolella@protonmail.com>
2024-08-26 17:11:13 +0200

    docu

--- src/players/players.module.ts
+++ src/players/players.module.ts
@@ -1,12 +1,12 @@
 import { Module, Global } from '@nestjs/common';
-import { HttpModule, HttpService } from '@nestjs/axios';
-
-export const AXIOS_INSTANCE_TOKEN = 'AXIOS_INSTANCE';
+import { HttpModule } from '@nestjs/axios';
+import { PlayersService } from './players.service';
 
 @Global()
 @Module({
   imports: [HttpModule],
-  exports: [AXIOS_INSTANCE_TOKEN],
+  providers: [PlayersService],
+  exports: [PlayersService],
 })
 export class PlayersModule {}
 
