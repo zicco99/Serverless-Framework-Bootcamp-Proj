d10537564f768cedddb5c98de0e0f291aa5ca91e
Francesco Ziccolella <f.ziccolella@protonmail.com>
2024-08-26 17:34:48 +0200

    controller attached

--- src/players/players.module.ts
+++ src/players/players.module.ts
@@ -2,10 +2,12 @@ import { Module, Global } from '@nestjs/common';
 import { HttpModule } from '@nestjs/axios';
 import { PlayersService } from './players.service';
 import { SportmonksService } from 'src/services/sport-monk.service';
+import { PlayersController } from './players.controller';
 
 @Global()
 @Module({
   imports: [HttpModule],
+  controllers: [PlayersController],
   providers: [PlayersService, SportmonksService],
   exports: [PlayersService],
 })
