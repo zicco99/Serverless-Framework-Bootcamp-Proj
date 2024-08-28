4a282e2893beb7556f0c0bc2535f374239dcb7c0
Francesco Ziccolella <f.ziccolella@protonmail.com>
2024-08-28 11:18:27 +0200

    log errors

--- src/main.ts
+++ src/main.ts
@@ -20,7 +20,7 @@ async function bootstrapServer(): Promise<Server> {
   const expressApp = require('express')();
   const adapter = new ExpressAdapter(expressApp);
 
-  return NestFactory.create(AppModule, adapter, { logger: ['debug', 'log'] })
+  return NestFactory.create(AppModule, adapter, { logger: ['debug', 'log', 'error', 'warn'] })
     .then((app) => app.init())
     .then(() => serverless.createServer(expressApp));
 }
