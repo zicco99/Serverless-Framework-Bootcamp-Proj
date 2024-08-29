9e5c35c1ce097c122cf142c73a5be2116cc765f0
Francesco Ziccolella <f.ziccolella@protonmail.com>
2024-08-29 16:15:09 +0200

    fix hook gen

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,4 @@
+// src/lambda.ts
 import { Handler, Context, APIGatewayProxyEvent } from 'aws-lambda';
 import { Server } from 'http';
 import { NestFactory } from '@nestjs/core';
@@ -5,7 +6,7 @@ import { AppModule } from './app.module';
 import { ExpressAdapter } from '@nestjs/platform-express';
 import * as serverless from 'aws-serverless-express';
 import { proxy } from 'aws-serverless-express';
-import { getBotToken,  } from 'nestjs-telegraf';
+import { Telegraf } from 'telegraf';
 
 let cachedServer: Server;
 
@@ -18,27 +19,48 @@ process.on('uncaughtException', (err) => {
 });
 
 async function bootstrapServer(webhookCallbackBaseUrl: string): Promise<Server> {
-  console.log("webhookCallbackBaseUrl", webhookCallbackBaseUrl);
+  console.log("Webhook Callback Base URL:", webhookCallbackBaseUrl);
 
   const expressApp = require('express')();
-
-  const botToken = process.env.BOT_TELEGRAM_KEY; 
-  if (!botToken) {
-    throw new Error('BOT_TELEGRAM_KEY is not defined');
-  }
-
   const app = await NestFactory.create(AppModule, new ExpressAdapter(expressApp), {
     logger: ['debug', 'log', 'error', 'warn'],
   });
 
-  const bot = app.get(getBotToken()); 
+  const bot = app.get(Telegraf);
+  if (!bot) {
+    throw new Error('Telegraf instance is not available');
+  }
+
+  // Use webhookCallback middleware
   app.use(bot.webhookCallback('/webhook'));
 
   try {
+    const webhookInfo = await bot.telegram.getWebhookInfo();
+    if (webhookInfo.url && webhookInfo.url !== `${webhookCallbackBaseUrl}/webhook`) {
+      console.log('Existing webhook URL detected. Deleting existing webhook.');
+      await bot.telegram.deleteWebhook();
+    }
+  } catch (error) {
+    console.error('Error getting or deleting existing webhook:', error);
+  }
+  try {
     await bot.telegram.setWebhook(`${webhookCallbackBaseUrl}/webhook`, {
       drop_pending_updates: true,
-      allowed_updates: ['message', 'edited_message', 'channel_post', 'edited_channel_post'],
+      allowed_updates: [
+        'message',
+        'edited_message',
+        'channel_post',
+        'edited_channel_post',
+        'callback_query',
+        'inline_query',
+        'chosen_inline_result',
+        'shipping_query',
+        'pre_checkout_query',
+        'poll',
+        'poll_answer'
+      ],
     });
+    console.log('Webhook set successfully');
   } catch (error) {
     console.error('Error setting webhook:', error);
   }
