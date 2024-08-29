e50ae6ce8649427f9c7c3d2f144e9cb5f7d80515
Francesco Ziccolella <f.ziccolella@protonmail.com>
2024-08-29 19:54:23 +0200

    process env variables will be every time defined

--- src/main.ts
+++ src/main.ts
@@ -17,12 +17,10 @@ process.on('unhandledRejection', function (reason, promise) {
   console.error("Unhandled Rejection at:", promise, "reason:", reason);
 });
 
-async function bootstrapServer(webhookCallbackBaseUrl: string): Promise<Server> {
+async function bootstrapServer(): Promise<Server> {
+
   if (cachedServer) return cachedServer;
   const expressApp = require('express')();
-
-  process.env.WEBHOOK_DOMAIN = webhookCallbackBaseUrl;
-  process.env.WEBHOOK_PATH = '/webhook';
 
   const app = await NestFactory.create(AppModule, new ExpressAdapter(expressApp), {
     logger: ['debug', 'log', 'error', 'warn'],
@@ -33,7 +31,7 @@ async function bootstrapServer(webhookCallbackBaseUrl: string): Promise<Server> 
     throw new Error('Bot Istance is not available');
   }
 
-  app.use(bot.webhookCallback('/webhook'));
+  app.use(bot.webhookCallback(process.env.WEBHOOK_PATH || "/webhook"));
 
   await app.init();
 
@@ -44,10 +42,13 @@ async function bootstrapServer(webhookCallbackBaseUrl: string): Promise<Server> 
 export const handler: Handler = async (event: APIGatewayProxyEvent, context: Context) => {
   console.log("-- Event:", event);
 
+  const webhookCallbackBaseUrl = `https://${event.requestContext.domainName}/${event.requestContext.stage}`;
+  process.env.WEBHOOK_DOMAIN = webhookCallbackBaseUrl;
+  process.env.WEBHOOK_PATH = '/webhook';
+
   if (!cachedServer) {
     try {
-      const webhookCallbackBaseUrl = `https://${event.requestContext.domainName}/${event.requestContext.stage}`;
-      cachedServer = await bootstrapServer(webhookCallbackBaseUrl);
+      cachedServer = await bootstrapServer();
     } catch (error) {
       console.error('Error bootstrapping server:', error);
       return {
