9f9932b558875ce3d182e6c5709b23dc594d7577
Francesco Ziccolella <f.ziccolella@protonmail.com>
2024-08-29 18:36:33 +0200

    init and configure

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,15 +1,10 @@
-// src/app.module.ts
 import { Module } from '@nestjs/common';
 import { AppService } from './app.service';
 import { TelegrafModule } from 'nestjs-telegraf';
 
 @Module({
   imports: [
-    TelegrafModule.forRoot(
-      {
-        token: process.env.BOT_TELEGRAM_KEY || "",
-      }
-    ),
+    TelegrafModule
   ],
   controllers: [],
   providers: [AppService],
--- src/main.ts
+++ src/main.ts
@@ -21,80 +21,54 @@ process.on('unhandledRejection', function (error, p) {
 
 async function bootstrapServer(webhookCallbackBaseUrl: string): Promise<Server> {
   
-  const curlCommand = `curl -X POST https://api.telegram.org/bot${process.env.BOT_TELEGRAM_KEY}/setWebhook \
-  -H "Content-Type: application/json" \
-  -d '{
-    "url": "${webhookCallbackBaseUrl}",
-    "drop_pending_updates": true,
-    "allowed_updates": [
-      "message",
-      "edited_message",
-      "channel_post",
-      "edited_channel_post",
-      "callback_query",
-      "inline_query",
-      "chosen_inline_result",
-      "shipping_query",
-      "pre_checkout_query",
-      "poll",
-      "poll_answer"
-    ]
-  }'`;
-
-  console.log('Manually setting webhook with curl command:', curlCommand);
-
   const expressApp = require('express')();
   const app = await NestFactory.create(AppModule, new ExpressAdapter(expressApp), {
     logger: ['debug', 'log', 'error', 'warn'],
   });
+  await app.init();
 
   const bot = app.get(getBotToken());
   if (!bot) {
-    throw new Error('Bot Istance is not available');
+    throw new Error('Bot instance is not available');
   }
 
-  app.use(bot.webhookCallback('/webhook'));
+  const desiredWebhookUrl = `${webhookCallbackBaseUrl}/webhook`;
 
-  if (!webhookSet) {
-    const desiredWebhookUrl = `${webhookCallbackBaseUrl}/webhook`;
+  try {
+    const webhookInfo = await bot.telegram.getWebhookInfo();
+    console.log("Current Webhook Info:", webhookInfo);
 
-    try {
-      const webhookInfo = await bot.telegram.getWebhookInfo();
-      console.log("Current Webhook Info:", webhookInfo);
-
-      // Only set the webhook if it's not already set correctly
-      if (webhookInfo.url !== desiredWebhookUrl) {
-        console.log('Setting new webhook to:', desiredWebhookUrl);
-        await bot.telegram.setWebhook(desiredWebhookUrl, {
-          drop_pending_updates: true,
-          allowed_updates: [
-            'message',
-            'edited_message',
-            'channel_post',
-            'edited_channel_post',
-            'callback_query',
-            'inline_query',
-            'chosen_inline_result',
-            'shipping_query',
-            'pre_checkout_query',
-            'poll',
-            'poll_answer'
-          ],
-        });
-        console.log('Webhook set successfully');
-        webhookSet = true;  // Mark that the webhook has been set
-      } else {
-        console.log('Webhook is already set correctly.');
-        webhookSet = true;  // Mark that the webhook has been checked and is correct
-      }
-    } catch (error) {
-      console.error('Error setting or fetching webhook info:', error);
+    if (webhookInfo.url !== desiredWebhookUrl) {
+      console.log('Setting new webhook to:', desiredWebhookUrl);
+      await bot.telegram.setWebhook(desiredWebhookUrl, {
+        drop_pending_updates: true,
+        allowed_updates: [
+          'message',
+          'edited_message',
+          'channel_post',
+          'edited_channel_post',
+          'callback_query',
+          'inline_query',
+          'chosen_inline_result',
+          'shipping_query',
+          'pre_checkout_query',
+          'poll',
+          'poll_answer'
+        ],
+      });
+      console.log('Webhook set successfully');
+    } else {
+      console.log('Webhook is already set correctly.');
     }
+
+    webhookSet = true;
+  } catch (error) {
+    console.error('Error setting or fetching webhook info:', error);
   }
 
-  await app.init();
   return serverless.createServer(expressApp);
 }
+
 
 export const handler: Handler = async (event: APIGatewayProxyEvent, context: Context) => {
   console.log("-- Event:", event);
