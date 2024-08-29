f8a6ccc569c0b43f1fb40b5826437b2595e82ce9
Francesco Ziccolella <f.ziccolella@protonmail.com>
2024-08-29 13:23:34 +0200

    removing the 2 modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,14 +1,10 @@
 // src/app.module.ts
 import { Module } from '@nestjs/common';
 import { AppService } from './app.service';
-import { AuctionsModule } from './auctions/auctions.module';
-import { TeamsModule } from './teams/teams.module';
 import { TelegrafModule } from 'nestjs-telegraf';
 
 @Module({
   imports: [
-    AuctionsModule,
-    TeamsModule,
     TelegrafModule.forRoot(
       {
         token: process.env.BOT_TELEGRAM_KEY || "",
