1f02a3b6b0a5a4b819d2c01087445b480ff7dc30
Francesco Ziccolella <f.ziccolella@protonmail.com>
2024-08-29 21:45:13 +0200

    fix?

--- src/main.ts
+++ src/main.ts
@@ -43,7 +43,7 @@ export const handler: Handler = async (event: APIGatewayProxyEvent, context: Con
 
   if (!cachedServer) {
     try {
-      console.log("Set manually : https://api.telegram.org/bot" + process.env.BOT_TELEGRAM_KEY + "/setWebhook?url=" + process.env.GATEWAY_URL + "/dev/webhook");
+      console.log("Set manually : https://api.telegram.org/bot" + process.env.BOT_TELEGRAM_KEY + "/setWebhook?url=" + process.env.GATEWAY_URL + "/webhook");
       cachedServer = await bootstrapServer();
     } catch (error) {
       console.error('Error bootstrapping server:', error);
