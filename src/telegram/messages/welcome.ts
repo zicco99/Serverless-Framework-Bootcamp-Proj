51487a17e7196faf2c9758d5541ddfa3bc25a6be
Francesco Ziccolella <f.ziccolella@protonmail.com>
2024-09-02 09:14:25 +0200

    replace

--- src/telegram/messages/welcome.ts
+++ src/telegram/messages/welcome.ts
@@ -3,12 +3,12 @@ import { escapeMarkdown } from './.utils';
 export const welcomeMessage = (userName: string, auctionsCounts: number): string => {
     const currentTime = new Date().toLocaleString();
 
-    let message = `*Hi, ${escapeMarkdown(userName)}\!*\n\n` +
+    let message = `*Hi, ${escapeMarkdown(userName)}\\!*\n\n` +
                   `Here are some quick stats for you:\n` +
                   `\- *Current Time:* ${escapeMarkdown(currentTime)} 🕖\n` +
                   `\- *Auctions Available:* ${escapeMarkdown(auctionsCounts.toString())} 🔥\n\n` +
                   `*﹎﹎﹎﹎﹎﹎﹎*\n\n` +
-                  `Welcome to the ultimate auctions manager bot\! 🎉\n` +
+                  `Welcome to the ultimate auctions manager bot\\! 🎉\n` +
                   `Get started by using /help and /userguide 📘\.\n\n` +
                   `*﹎﹎﹎﹎﹎﹎﹎*\n\n` +
                   `Wishing you peace and love ❤️\n` +
