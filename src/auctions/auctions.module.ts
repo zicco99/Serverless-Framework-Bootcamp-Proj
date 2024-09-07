470379ba24b86e777bacc61f4a9807e965192e7a
Francesco Ziccolella <f.ziccolella@protonmail.com>
2024-09-07 12:16:11 +0200

    link also redis

--- src/auctions/auctions.module.ts
+++ src/auctions/auctions.module.ts
@@ -1,11 +1,12 @@
 import { Global, Module } from '@nestjs/common';
 import { AuctionsService } from './auctions.service';
 import { AuctionWizard } from './wizards/create-auction.wizard';
+import { RedisClusterService } from 'src/services/redis/redis-custer.service';
 
 @Global()
 @Module({
-  providers: [AuctionsService, AuctionWizard],
-  exports: [AuctionsService, AuctionWizard],
+  providers: [AuctionsService, AuctionWizard, RedisClusterService, ],
+  exports: [AuctionsService, AuctionWizard, RedisClusterService],
 })
 export class AuctionsModule {}
 
