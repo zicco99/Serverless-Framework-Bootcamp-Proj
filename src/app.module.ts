77992edc452e5179f208b9fcb85a830a02e062e1
Francesco Ziccolella <f.ziccolella@protonmail.com>
2024-09-07 13:25:10 +0200

    f

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,13 +2,16 @@ import { Module } from '@nestjs/common';
 import { TelegrafModule, TelegrafModuleOptions } from 'nestjs-telegraf';
 import { AppService } from './app.service';
 import { InjectSessionSpaceMiddleware } from './telegram/middlewares/session-space.middleware';
-import { AuctionWizard } from './auctions/wizards/create-auction.wizard'; 
-import { RedisClusterService } from 'src/services/redis/redis-custer.service'; 
+import { AuctionsModule } from './auctions/auctions.module';
 import { session } from 'telegraf';
+import { AuctionWizard } from './auctions/wizards/create-auction.wizard';
+import { RedisClusterService } from './services/redis/redis-custer.service';
 
 @Module({
   imports: [
+    AuctionsModule,
     TelegrafModule.forRootAsync({
+      imports: [AuctionsModule],
       inject: [AuctionWizard, RedisClusterService],
       useFactory: (auctionWizard: AuctionWizard, redisService: RedisClusterService): TelegrafModuleOptions => ({
         token: process.env.BOT_TELEGRAM_KEY || '',
@@ -41,6 +44,6 @@ import { session } from 'telegraf';
     }),
   ],
   controllers: [],
-  providers: [AppService, AuctionWizard, RedisClusterService],
+  providers: [AppService],
 })
 export class AppModule {}
